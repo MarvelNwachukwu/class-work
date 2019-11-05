a11886b36406789751a10b929f2aac1b3e9ee650
MarvelNwachukwu <marvellousn561@gmail.com>
2019-11-05 12:18:32 +0100

    added warning

--- Myscript.js
+++ Myscript.js
@@ -1,2 +1,2 @@
-alert( "This website uses cookies");
-getelementsbyid("SteveImg");
+alert( "This website uses cookies\n We use cookies to personalise content and ads, to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you’ve provided to them or that they’ve collected from your use of their services");
+getelementsbyid("SteveImg").innerhtml = 5 + 6;
