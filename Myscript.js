e86d53b611e4db882b28ade545a4b3b1ef8aba8b
MarvelNwachukwu <marvellousn561@gmail.com>
2019-11-06 21:38:14 +0100

    Started inputing survey questions via javascript.

--- Myscript.js
+++ Myscript.js
@@ -1,2 +1,4 @@
 alert( "This website uses cookies\n We use cookies to personalise content and ads, to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you’ve provided to them or that they’ve collected from your use of their services");
-getelementsbyid("SteveImg").innerhtml = 5 + 6;
+var survey = prompt('What is your reason for visiting our website?');
+console.log (survey);
+// document.querySelector('.STEVEN').innerText = lastname;
